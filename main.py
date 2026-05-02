a87d72c9ee9d98e424a8c44152856bf308a39e95
denisababeii <babeii.denisa@yahoo.ro>
2026-05-02 12:38:18 +0200

    Update naming

--- main.py
+++ main.py
@@ -538,4 +538,4 @@ def health():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main_with_rag:app", host="0.0.0.0", port=8001)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001)
